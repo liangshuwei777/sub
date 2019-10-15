5dd0996b7d1e075b508d31f4594de4f60729e434
liangshuwei777 <55431886+liangshuwei777@users.noreply.github.com>
2019-10-15 14:07:08 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,57 @@
+public class Main {
+     public static void main(String[] args) {
+            read();
+            System.out.println("北京地铁查询");
+          while(true) {
+            write();
+            }
+        }
+        public static void read(){
+            DistanceBuilder.FILE_PATH=System.getProperty("user.dir") + File.separator + "\\" +"sub.txt";
+            DistanceBuilder.readSubway();
+        }
+        public static void write() {
+            Scanner input=new Scanner(System.in);
+            System.out.print("输入指令：查询地铁线路信息：-info 路线。查询起末站线路：-route 起始站 终点站:");
+             String s=input.nextLine();
+             String[] split =s.split("\\s+");
+             if(split[0].equals("-map")) {
+                 if(split.length==1){
+                     DistanceBuilder.readSubway();
+                       System.out.println("成功读取sub.txt文件");
+                   }else{
+                       System.out.println("重新输入");
+                   }
+             }
+             else if(split[0].equals("-info")) {
+                  if(split.length==2){
+                      DistanceBuilder.readSubway();
+                      DistanceBuilder.writeLineData(split[1]);
+                   }else{
+ 
+                       System.out.println("输入错误，请重新输入");
+                   }
+             }
+             else if(split[0].equals("-way")) {
+                   if(split.length==3){
+                       if(split[1].equals(split[2]))
+                           System.out.println("输入站点重复，请重新输入");
+                       else {
+                       DistanceBuilder.readSubway();
+                       System.out.println(split[1]+" -> "+split[2]+":");
+                       Result result = DijkstraUtil.calculate(new Station(split[1]), new Station(split[2]));
+                       //System.out.println(split[2]);
+                       DistanceBuilder.getPassStation(result);
+                       //System.out.println(result);
+                       if(result.getDistance()==0.0)
+                           System.out.println("输入站点不存在");
+                   }
+                   }else{
+                       System.out.println("输入错误，请重新输入");
+                   }
+                    
+              
+             }
+                System.out.println();
+        }
+}
